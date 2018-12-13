3cf3138d798390c68dad2136709578d527f5ee67
Eugene Y. Q. Shen <eugene@eyqs.ca>
2018-12-12 20:27:47 -0800

    Clean up pde/simulation.py

--- pde/simulation.py
+++ pde/simulation.py
@@ -1,88 +1,80 @@
 import control
 import numpy as np
-from collections import OrderedDict
 from copy import deepcopy
 from pde.model import Model
 
+
 class Simulation:
 
-    # Initialize with simulation of 10 units time
     MIN_TIME = 10
-    # Maximum possible simulation time
     MAX_TIME = 1000
 
-    def __init__(self, model, uid):
+
+    def __init__(self, model, sim_id):
         self.model = model
-        self.id = uid
+        self.sim_id = sim_id
         self.reset()
 
+
     def reset(self):
-        self.simulation = None
         self.active = False
+        self.simulation = None
         self.time = np.linspace(
-            0, Simulation.MIN_TIME - 1, Simulation.MIN_TIME)
+                0, Simulation.MIN_TIME - 1, Simulation.MIN_TIME)
         self.inputs = np.zeros((self.model.system.inputs, Simulation.MIN_TIME))
-        print('Simulation #{} created. Call activate() to activate'.format(self.id))
+        print(f'Simulation {self.sim_id} of {self.model.name} created. ' +
+                'Call `activate()` to activate.')
+
 
     def activate(self):
-        if not self.active:
+        if self.active:
+            assert self.simulation
+            print(f'Simulation {self.sim_id} already active. ' +
+                    'Call `reset()` to deactivate.')
+        else:
             self.active = True
             self.simulation = self.step_generator()
-            self.simulation.send(None)
-            print('Simulation #{} activated. Ready to simulate'.format(self.id))
-        else:
-            print(
-                'Simulation #{} already active. Call reset() to deactivate'.format(self.id))
+            print(f'Simulation {self.sim_id} activated. '+
+                    'Call `step()` to run the simulation.')
 
-    def updateTag(self, inputs):
+
+    def update_tag(self, inputs):
         if not isinstance(inputs, dict):
-            raise TypeError("Input must be a dictionary of tags to update")
+            raise TypeError('Input must be a dictionary of tags to update.')
         for key in inputs:
             try:
                 self.model.inputs[key].value = inputs[key]
-                print('Updated {0} to: {1}'.format(key, inputs[key]))
+                print(f'Updated tag {key} to: {inputs[key]}.')
             except KeyError:
-                print('Tag not found in inputs: {}'.format(key))
+                print(f'Tag {key} not found in inputs.')
 
+
+    # TODO: Decide how to handle first few data points in MIN_TIME
     def step(self):
         if not self.active:
-            raise ValueError("Simulation needs to be initialized first")
-        assert not self.simulation is None
+            raise ValueError(f'Simulation {self.sim_id} must be activated.')
+        assert self.simulation
         try:
-            T, Y = next(self.simulation)
-            last_Y = Y[:, -1]
+            # Get new outputs from the simulation step
+            T, yout = next(self.simulation)
+
+            # We only use the last (newest) value in yout to update our output
+            # TODO: Switch to state space for iterative calculations
             keys = list(self.model.outputs.keys())
-            for idx, y in enumerate(last_Y):
-                self.model.outputs[keys[idx]] = y
+            for i, y in enumerate(yout[:, -1]):
+                self.model.outputs[keys[i]].value = y
             return deepcopy(self.model.outputs)
+
         except StopIteration:
-            print('Error: Simulation not active or exceeded maximum simulation time.')
+            print('Error: Simulation not active or ' +
+                    'exceeded maximum simulation time.')
+
 
     def step_generator(self):
-        while (self.active) and (self.time[-1] <= Simulation.MAX_TIME):
-            inputs = [i.value for i in self.model.inputs.values()]
-            self.inputs = np.column_stack((self.inputs, inputs))
+        while self.active and self.time[-1] < Simulation.MAX_TIME:
             self.time = np.append(self.time, self.time[-1] + 1)
-            T, Y, _ = control.forced_response(
-                self.model.system, self.time, self.inputs)
-            yield (T, Y)
-
-    @property
-    def model(self):
-        return self._model
-
-    @model.setter
-    def model(self, model):
-        if not isinstance(model, Model):
-            raise TypeError("Simulation model must be of type Model")
-        self._model = model
-
-    @property
-    def id(self):
-        return self._id
-
-    @id.setter
-    def id(self, value):
-        if not value:
-            raise ValueError("Simulation ID cannot be empty")
-        self._id = value
+            self.inputs = np.column_stack((self.inputs,
+                [tag.value for tag in self.model.inputs.values()]))
+            T, yout, xout = control.forced_response(
+                    self.model.system, self.time, self.inputs)
+            yield T, yout
